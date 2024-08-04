4ba46f9c0a184127ec0d3c2cf5c4f3cf9825df06
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-08-04 14:51:17 +0300

    tasks added

---
+++ src/main/java/org/example/C_0625_ListFiltering.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and
+returns a new list with the strings filtered out.
+
+Example
+Kata.filterList(List.of(1, 2, "a", "b")) => List.of(1,2)
+Kata.filterList(List.of(1, "a", "b", 0, 15)) => List.of(1,0,15)
+Kata.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 123)) => List.of(1, 2, 123)
+ */
+public class C_0625_ListFiltering {
+    public static void main(String[] args) {
+        System.out.println(filterList(List.of(1, 2, "a", "b")));
+        System.out.println(filterList(List.of(1, "a", "b", 0, 15)));
+        System.out.println(filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 123)));
+    }
+
+    private static List<Object> filterList(List<Object> input) {
+        List<Object> result = new ArrayList<>();
+        input.stream()
+                .filter(item -> item instanceof Integer)
+                .forEach(result::add);
+        return result;
+    }
+}
